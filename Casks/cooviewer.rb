6ce136ab963d57b3c673c765b6d7a82263d2aab3
ysmx <me+git@kikuyayoshimi.com>
2026-04-27 01:37:23 +0900

    Add cooviewer cask v1.3.9

---
+++ Casks/cooviewer.rb
@@ -0,0 +1,11 @@
+cask "cooviewer" do
+  version "1.3.9"
+  sha256 "cdde4e639917ba0cfd078469d31678e6cf12c441598dfd055088c083c828c181"
+
+  url "https://github.com/ysmx/cooViewer/releases/download/v#{version}/cooViewer.zip"
+  name "cooViewer"
+  desc "macOS向け画像ビューア（漫画・ZIP/RAR対応）"
+  homepage "https://github.com/ysmx/cooViewer"
+
+  app "cooViewer.app"
+end
